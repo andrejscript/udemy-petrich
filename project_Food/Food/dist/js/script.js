1260ebb3482c7df4e9e8a9ffe4cf6f0dcc888990
AndreWeb <andzion105@gmail.com>
2020-08-28 14:54:55 +0300

    end ex #41 timer to Food

--- project_Food/Food/dist/js/script.js
+++ project_Food/Food/dist/js/script.js
@@ -1,8 +1,12 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    
-// Tabs
+
+
+
+    //
+// TABS
+    //
 
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
@@ -42,22 +46,62 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-// Timer
 
-    const deadline = '2020-05-11';
+
+    //
+// TIMER
+    //
+    const deadline = '2020-09-30';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
-        days = Math.floor(t / (1000 * 60 * 60 * 24)),
-        hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-        minutes = Math.floor(( t / 1000 / 60) % 60),
-        seconds =  Math.floor(( t / 1000) % 60);
+        days = Math.floor( (t/(1000 * 60 * 60 * 24)) ),
+        hours = Math.floor( (t/(1000 * 60 * 60) % 24) ),
+        minutes = Math.floor( (t/1000 / 60) % 60 ),
+        seconds =  Math.floor( (t/1000) % 60 );
 
         return {
-            'total' : t,
+            'total': t,
+            days,
+            hours,
+            minutes,
+            seconds
             
+        };
+    }
+
+    function addZero(num) {
+        if(num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
         }
     }
 
+    function setClock(selector, endtime) {
 
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    
+    setClock('.timer', deadline);
 });
